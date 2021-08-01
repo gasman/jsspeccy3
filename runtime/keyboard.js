2804575448c1bad012ae747d8337ba1daaa4b158
Matt Westcott <matt@west.co.tt>
2021-08-01 13:30:27 +0100

    split keyboard handler into its own module

---
+++ runtime/keyboard.js
@@ -0,0 +1,106 @@
+const KEY_CODES = {
+    49: {row: 3, mask: 0x01}, /* 1 */
+    50: {row: 3, mask: 0x02}, /* 2 */
+    51: {row: 3, mask: 0x04}, /* 3 */
+    52: {row: 3, mask: 0x08}, /* 4 */
+    53: {row: 3, mask: 0x10}, /* 5 */
+    54: {row: 4, mask: 0x10}, /* 6 */
+    55: {row: 4, mask: 0x08}, /* 7 */
+    56: {row: 4, mask: 0x04}, /* 8 */
+    57: {row: 4, mask: 0x02}, /* 9 */
+    48: {row: 4, mask: 0x01}, /* 0 */
+
+    81: {row: 2, mask: 0x01}, /* Q */
+    87: {row: 2, mask: 0x02}, /* W */
+    69: {row: 2, mask: 0x04}, /* E */
+    82: {row: 2, mask: 0x08}, /* R */
+    84: {row: 2, mask: 0x10}, /* T */
+    89: {row: 5, mask: 0x10}, /* Y */
+    85: {row: 5, mask: 0x08}, /* U */
+    73: {row: 5, mask: 0x04}, /* I */
+    79: {row: 5, mask: 0x02}, /* O */
+    80: {row: 5, mask: 0x01}, /* P */
+
+    65: {row: 1, mask: 0x01}, /* A */
+    83: {row: 1, mask: 0x02}, /* S */
+    68: {row: 1, mask: 0x04}, /* D */
+    70: {row: 1, mask: 0x08}, /* F */
+    71: {row: 1, mask: 0x10}, /* G */
+    72: {row: 6, mask: 0x10}, /* H */
+    74: {row: 6, mask: 0x08}, /* J */
+    75: {row: 6, mask: 0x04}, /* K */
+    76: {row: 6, mask: 0x02}, /* L */
+    13: {row: 6, mask: 0x01}, /* enter */
+
+    16: {row: 0, mask: 0x01}, /* caps */
+    192: {row: 0, mask: 0x01}, /* backtick as caps - because firefox screws up a load of key codes when pressing shift */
+    90: {row: 0, mask: 0x02}, /* Z */
+    88: {row: 0, mask: 0x04}, /* X */
+    67: {row: 0, mask: 0x08}, /* C */
+    86: {row: 0, mask: 0x10}, /* V */
+    66: {row: 7, mask: 0x10}, /* B */
+    78: {row: 7, mask: 0x08}, /* N */
+    77: {row: 7, mask: 0x04}, /* M */
+    17: {row: 7, mask: 0x02}, /* sym - gah, firefox screws up ctrl+key too */
+    32: {row: 7, mask: 0x01}, /* space */
+
+    /* shifted combinations */
+    8: {row: 4, mask: 0x01, caps: true}, /* backspace => caps + 0 */
+    37: {row: 3, mask: 0x10, caps: true}, /* left arrow => caps + 5 */
+    38: {row: 4, mask: 0x08, caps: true}, /* up arrow => caps + 7 */
+    39: {row: 4, mask: 0x04, caps: true}, /* right arrow => caps + 8 */
+    40: {row: 4, mask: 0x10, caps: true}, /* down arrow => caps + 6 */
+};
+
+
+export class KeyboardHandler {
+    constructor(worker) {
+        this.worker = worker;
+
+        this.keydownHandler = (evt) => {
+            const keyCode = KEY_CODES[evt.keyCode];
+            if (keyCode) {
+                this.worker.postMessage({
+                    message: 'keyDown', row: keyCode.row, mask: keyCode.mask,
+                })
+                if (keyCode.caps) {
+                    this.worker.postMessage({
+                        message: 'keyDown', row: 0, mask: 0x01,
+                    })
+                }
+            }
+            if (!evt.metaKey) evt.preventDefault();
+        };
+
+        this.keyupHandler = (evt) => {
+            const keyCode = KEY_CODES[evt.keyCode];
+            if (keyCode) {
+                this.worker.postMessage({
+                    message: 'keyUp', row: keyCode.row, mask: keyCode.mask,
+                })
+                if (keyCode.caps) {
+                    this.worker.postMessage({
+                        message: 'keyUp', row: 0, mask: 0x01,
+                    })
+                }
+            }
+            if (!evt.metaKey) evt.preventDefault();
+        };
+
+        this.keypressHandler = (evt) => {
+            if (!evt.metaKey) evt.preventDefault();
+        };
+    }
+
+    start() {
+        document.addEventListener('keydown', this.keydownHandler);
+        document.addEventListener('keyup', this.keyupHandler);
+        document.addEventListener('keypress', this.keypressHandler);
+    }
+
+    stop() {
+        document.removeEventListener('keydown', this.keydownHandler);
+        document.removeEventListener('keyup', this.keyupHandler);
+        document.removeEventListener('keypress', this.keypressHandler);
+    }
+}
