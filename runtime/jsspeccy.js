4ad30fe11aa55e67239ec3fd7f4ff0270b7ccc5e
Matt Westcott <matt@west.co.tt>
2021-08-01 15:22:01 +0100

    remove option for disabling UI
    we'll add more fine-grained options for selectively enabling menu options / toolbar buttons later, and removing them all will have the same effect as running without a UI wrapper but without the annoying special-casing in the fullscreen logic

--- runtime/jsspeccy.js
+++ runtime/jsspeccy.js
@@ -174,20 +174,25 @@ window.JSSpeccy = (container, opts) => {
     // let benchmarkRenderCount = 0;
     opts = opts || {};
 
+    const appContainer = document.createElement('div');
+    container.appendChild(appContainer);
+    let menuBar = new MenuBar(appContainer);
+
     const canvas = document.createElement('canvas');
     canvas.width = 320;
     canvas.height = 240;
     canvas.style.objectFit = 'contain';
+    appContainer.appendChild(canvas);
+    canvas.style.display = 'block';
 
-    const appContainer = document.createElement('div');
-    container.appendChild(appContainer);
+    let toolbar = new Toolbar(appContainer);
+
+    const emu = new Emulator(canvas, opts.machine || 128);
 
     let zoom;
     let displayWidth;
     let displayHeight;
     let onSetZoom;
-    let menuBar = null;
-    let toolbar = null;
     let isFullscreen = false;
 
     const setZoom = (factor) => {
@@ -219,16 +224,16 @@ window.JSSpeccy = (container, opts) => {
         if (allowUIHiding && !uiIsHidden) {
             uiIsHidden = true;
             appContainer.style.cursor = 'none';
-            if (menuBar) menuBar.hide();
-            if (toolbar) toolbar.hide();
+            menuBar.hide();
+            toolbar.hide();
         }
     }
     const showUI = () => {
         if (uiIsHidden) {
             uiIsHidden = false;
             appContainer.style.cursor = 'default';
-            if (menuBar) menuBar.show();
-            if (toolbar) toolbar.show();
+            menuBar.show();
+            toolbar.show();
         }
     }
     let hideUITimeout = null;
@@ -250,114 +255,101 @@ window.JSSpeccy = (container, opts) => {
             document.addEventListener('mousemove', fullscreenMouseMove);
             /* a bogus mousemove event is emitted on entering fullscreen, so ignore it */
             ignoreNextMouseMove = true;
-            if (menuBar) {
-                menuBar.enterFullscreen();
-                menuBar.onmouseenter(() => {allowUIHiding = false;});
-                menuBar.onmouseout(() => {allowUIHiding = true;});
-            }
-            if (toolbar) {
-                toolbar.enterFullscreen();
-                toolbar.onmouseenter(() => {allowUIHiding = false;});
-                toolbar.onmouseout(() => {allowUIHiding = true;});
-            }
+
+            menuBar.enterFullscreen();
+            menuBar.onmouseenter(() => {allowUIHiding = false;});
+            menuBar.onmouseout(() => {allowUIHiding = true;});
+
+            toolbar.enterFullscreen();
+            toolbar.onmouseenter(() => {allowUIHiding = false;});
+            toolbar.onmouseout(() => {allowUIHiding = true;});
+
             hideUI();
             if (onSetZoom) onSetZoom('fullscreen');
         } else {
             isFullscreen = false;
             if (hideUITimeout) clearTimeout(hideUITimeout);
             showUI();
-            if (menuBar) {
-                menuBar.exitFullscreen();
-                menuBar.onmouseenter(null);
-                menuBar.onmouseout(null);
-            }
-            if (toolbar) {
-                toolbar.exitFullscreen();
-                toolbar.onmouseenter(null);
-                toolbar.onmouseout(null);
-            }
+
+            menuBar.exitFullscreen();
+            menuBar.onmouseenter(null);
+            menuBar.onmouseout(null);
+
+            toolbar.exitFullscreen();
+            toolbar.onmouseenter(null);
+            toolbar.onmouseout(null);
+
             document.removeEventListener('mousemove', fullscreenMouseMove);
             setZoom(zoom);
         }
     })
 
-    const emu = new Emulator(canvas, opts.machine || 128);
+    const fileMenu = menuBar.addMenu('File');
+    fileMenu.addItem('Open...', () => {
+        openFileDialog();
+    });
+    const machineMenu = menuBar.addMenu('Machine');
+    const machine48Item = machineMenu.addItem('Spectrum 48K', () => {
+        emu.setMachine(48);
+    });
+    const machine128Item = machineMenu.addItem('Spectrum 128K', () => {
+        emu.setMachine(128);
+    });
+    const displayMenu = menuBar.addMenu('Display');
 
-    if (opts.ui) {
-        menuBar = new MenuBar(appContainer);
-        const fileMenu = menuBar.addMenu('File');
-        fileMenu.addItem('Open...', () => {
-            openFileDialog();
-        });
-        const machineMenu = menuBar.addMenu('Machine');
-        const machine48Item = machineMenu.addItem('Spectrum 48K', () => {
-            emu.setMachine(48);
-        });
-        const machine128Item = machineMenu.addItem('Spectrum 128K', () => {
-            emu.setMachine(128);
-        });
-        const displayMenu = menuBar.addMenu('Display');
-
-        const zoomItemsBySize = {
-            1: displayMenu.addItem('100%', () => setZoom(1)),
-            2: displayMenu.addItem('200%', () => setZoom(2)),
-            3: displayMenu.addItem('300%', () => setZoom(3)),
-        }
-        const fullscreenItem = displayMenu.addItem('Fullscreen', () => {
-            enterFullscreen();
-        })
-        onSetZoom = (factor) => {
-            if (factor == 'fullscreen') {
-                fullscreenItem.setCheckbox();
-                for (let i in zoomItemsBySize) {
+    const zoomItemsBySize = {
+        1: displayMenu.addItem('100%', () => setZoom(1)),
+        2: displayMenu.addItem('200%', () => setZoom(2)),
+        3: displayMenu.addItem('300%', () => setZoom(3)),
+    }
+    const fullscreenItem = displayMenu.addItem('Fullscreen', () => {
+        enterFullscreen();
+    })
+    onSetZoom = (factor) => {
+        if (factor == 'fullscreen') {
+            fullscreenItem.setCheckbox();
+            for (let i in zoomItemsBySize) {
+                zoomItemsBySize[i].unsetCheckbox();
+            }
+        } else {
+            fullscreenItem.unsetCheckbox();
+            for (let i in zoomItemsBySize) {
+                if (parseInt(i) == factor) {
+                    zoomItemsBySize[i].setCheckbox();
+                } else {
                     zoomItemsBySize[i].unsetCheckbox();
                 }
-            } else {
-                fullscreenItem.unsetCheckbox();
-                for (let i in zoomItemsBySize) {
-                    if (parseInt(i) == factor) {
-                        zoomItemsBySize[i].setCheckbox();
-                    } else {
-                        zoomItemsBySize[i].unsetCheckbox();
-                    }
-                }
             }
         }
+    }
 
-        emu.on('setMachine', (type) => {
-            if (type == 48) {
-                machine48Item.setCheckbox();
-                machine128Item.unsetCheckbox();
+    emu.on('setMachine', (type) => {
+        if (type == 48) {
+            machine48Item.setCheckbox();
+            machine128Item.unsetCheckbox();
+        } else {
+            machine48Item.unsetCheckbox();
+            machine128Item.setCheckbox();
+        }
+    });
+
+    toolbar.addButton(openIcon, {label: 'Open file'}, () => {
+        openFileDialog();
+    });
+    toolbar.addButton(resetIcon, {label: 'Reset'}, () => {
+        emu.reset();
+    });
+    toolbar.addButton(
+        fullscreenIcon,
+        {label: 'Enter full screen mode', align: 'right'},
+        () => {
+            if (isFullscreen) {
+                exitFullscreen();
             } else {
-                machine48Item.unsetCheckbox();
-                machine128Item.setCheckbox();
+                enterFullscreen();
             }
-        });
-    }
-
-    appContainer.appendChild(canvas);
-    canvas.style.display = 'block';
-
-    if (opts.ui) {
-        toolbar = new Toolbar(appContainer);
-        toolbar.addButton(openIcon, {label: 'Open file'}, () => {
-            openFileDialog();
-        });
-        toolbar.addButton(resetIcon, {label: 'Reset'}, () => {
-            emu.reset();
-        });
-        toolbar.addButton(
-            fullscreenIcon,
-            {label: 'Enter full screen mode', align: 'right'},
-            () => {
-                if (isFullscreen) {
-                    exitFullscreen();
-                } else {
-                    enterFullscreen();
-                }
-            }
-        )
-    }
+        }
+    )
 
     setZoom(opts.zoom || 1);
 
